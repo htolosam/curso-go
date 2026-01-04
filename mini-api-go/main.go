a836aa78a3dd7fa22a546eb9de0608dee8f341bc
ho tolosa <htolosam@gmail.com>
2026-01-04 04:49:28 -0500

    add server mux

--- mini-api-go/main.go
+++ mini-api-go/main.go
@@ -1,5 +1,20 @@
 package main
 
+import (
+	"log"
+	"mini-api-go/handlers"
+	"net/http"
+)
+
 func main() {
-	
+	mux := http.NewServeMux()
+	mux.HandleFunc("GET /health", handlers.Health)
+	mux.HandleFunc("GET /posts", handlers.GetPosts)
+	mux.HandleFunc("GET /posts/{id}", handlers.GetPostByID)
+	mux.HandleFunc("POST /posts", handlers.CreatePost)
+	log.Println("server in 8080")
+	err := http.ListenAndServe(":8080", mux)
+	if err != nil {
+		return
+	}
 }
