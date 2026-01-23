ea2042790f3527a917eb56e4a5cfe3c643d7a8e7
htolosam <htolosam@gmail.com>
2026-01-22 23:58:00 -0500

    add post handler and service

--- mini-api-go/models/post.go
+++ mini-api-go/models/post.go
@@ -2,9 +2,9 @@ package models
 
 type Post struct {
 	ID        uint   `json:"id"`
-	UserID    uint   `json:"user_id"`
-	Tittle    string `json:"tittle"`
-	Content   string `json:"content"`
+	UserID    uint   `json:"user_id" validate:"required"`
+	Tittle    string `json:"tittle" validate:"required"`
+	Content   string `json:"content" validate:"required"`
 	Author    string `json:"author"`
 	CreatedAt string `json:"created_at"`
 	UpdatedAt string `json:"updated_at"`
